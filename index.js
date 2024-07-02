75ef6abe12a8ae59da6b9b08ce754a0b9a2cdda7
girl-on-a-string <frenchfries55@protonmail.com>
2024-07-02 14:25:33 -0500

    removed add task button and converted it to an input field

---
+++ index.js
@@ -0,0 +1,22 @@
+//variables 
+
+const all = document.querySelector("all");
+const container = document.querySelector("container");
+const addTask = document.querySelector("add-task");
+
+const list = container.querySelector("list");
+
+//create task
+
+function createTask () {
+    console.log("received task");
+    addTask.addEventListener("click", {
+
+    });
+}
+
+//populate interface with task
+
+function populateInterface () {
+    console.log("received request");
+}
