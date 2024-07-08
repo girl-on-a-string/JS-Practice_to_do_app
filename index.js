e60c262eca679d68587004810b9f4a7f260a05bb
girl-on-a-string <frenchfries55@protonmail.com>
2024-07-08 12:24:21 -0500

    null input error implimented

--- index.js
+++ index.js
@@ -10,24 +10,22 @@ const list = container.querySelector("#list");
 //create task
 
 function createTask () {
+    console.log("received input");
     let listText = document.getElementById("input").value;
     const listItem = document.createElement("li");
     listItem.innerHTML = listText;
     list.appendChild(listItem);
     console.log(true);
-        
-    console.log("received input");
-
 }
+
+//display task or throw error
 
 add.addEventListener("submit", (e) => {
-    e.preventDefault();
-    createTask();
-    addTask.value = null;
+    if (addTask.value == "" || addTask.value == null) {
+        alert("You can't have an empty task!");
+    } else {
+        e.preventDefault();
+        createTask();
+        addTask.value = null;
+    }
 });
-
-//populate interface with task
-
-function finishTask () {
-    console.log("received request");
-}
